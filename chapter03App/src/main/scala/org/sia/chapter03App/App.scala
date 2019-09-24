ec463531bc50bb857d6994e1fff7b93f116b86a2
dbdu <1797890817@qq.com>
2019-09-24 21:06:19 +0800

    文件分发

--- chapter03App/src/main/scala/org/sia/chapter03App/App.scala
+++ chapter03App/src/main/scala/org/sia/chapter03App/App.scala
@@ -1,27 +1,42 @@
 package org.sia.chapter03App
 
 import org.apache.spark.sql.SparkSession
+import scala.io.Source.fromFile
 
-/**
- * @author ${user.name}
- */
 object App {
 
   def main(args : Array[String]) {
+    // TODO expose appName and master as app. params
     val spark = SparkSession.builder()
-      .appName("The swankiest Spark app ever")
-      .master("local[*]")
-      .getOrCreate()
+        .appName("GitHub push counter")
+        .master("local[*]")
+        .getOrCreate()
 
     val sc = spark.sparkContext
 
-    val col = sc.parallelize(0 to 100 by 5)
-    val smp = col.sample(true, 4)
-    val colCount = col.count
-    val smpCount = smp.count
+    // TODO expose inputPath as app. param
+    val homeDir = System.getenv("HOME")
+    val inputPath = homeDir + "/sia/github-archive/2015-03-01-0.json"
+    val ghLog = spark.read.json(inputPath)
 
-    println("orig count = " + colCount)
-    println("sampled count = " + smpCount)
+    val pushes = ghLog.filter("type = 'PushEvent'")
+    val grouped = pushes.groupBy("actor.login").count
+    val ordered = grouped.orderBy(grouped("count").desc)
+
+      // TODO expose empPath as app. param
+    val empPath = "/media/dbdu/Data/repository/GitHub/first-edition/ch03/ghEmployees.txt"
+    val employees = Set() ++ (
+      for {
+        line <- fromFile(empPath).getLines
+      } yield line.trim
+    )
+    val bcEmployees = sc.broadcast(employees)   // A Broadcast the             employees set
+
+
+    import spark.implicits._
+    val isEmp = user => bcEmployees.value.contains(user)
+    val isEmployee = spark.udf.register("SetContainsUdf", isEmp)
+    val filtered = ordered.filter(isEmployee($"login"))
+    filtered.show()
   }
-
 }
